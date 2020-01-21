b2c4392dde0e15d661b50f7b3c4df873642c93ec
Amar Maharjan <amar.maharjan410@gmail.com>
2020-01-21 11:10:11 +0545

    restaurant management system

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -5,7 +5,7 @@
 @push('css')
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
 
-@endpush
+@endpush 
 
 @section('content')
 	<div class="content">
@@ -37,7 +37,7 @@
                       		<td>{{ $key + 1 }}</td>
                       		<td>{{ $slider->title }}</td>
                       		<td>{{ $slider->sub_title }}</td>
-                      		<td>{{ $slider->image }}</td>
+                      		<td><img class="img-responsive img-thumbnail" src="{{ asset('uploads/slider/'.$slider->image) }}" style="height: 100px; width: 100px" alt=""></td>
                       		<td>{{ $slider->created_at }}</td>
                       		<td>{{ $slider->updated_at }}</td>
                       		<td><a href="{{ route('slider.edit', $slider->id) }}" class="btn btn-info btn-sm"><i class="material-icons">mode_edit</i></a>
